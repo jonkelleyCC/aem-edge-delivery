e3260b3d2975e9ee2ef0b06c0f0af5ba089811be
Jon Kelley <jkelley@crescendocollective.com>
2024-11-07 16:19:52 -0600

    day description js test

---
+++ blocks/day-description/day-description.js
@@ -0,0 +1,29 @@
+// import {
+//   decorateMain,
+// } from '../../scripts/scripts.js';
+
+// import {
+//   loadSections,
+// } from '../../scripts/aem.js';
+
+export async function loadFragment(path) {
+  if (path && path.startsWith('/')) {
+    // eslint-disable-next-line no-param-reassign
+    path = path.replace(/(\.plain)?\.html/, '').replace('/content/dam', '');
+    const resp = await fetch(`/api/assets${path}.json`);
+    if (resp.ok) {
+      return resp.json();
+    }
+  }
+  return null;
+}
+
+export default async function decorate(block) {
+  const link = block.querySelector('a');
+  const path = link ? link.getAttribute('href') : block.textContent.trim();
+  const fragment = await loadFragment(path);
+  if (fragment) {
+    const container = block.querySelector('>div');
+    console.log(fragment);
+  }
+}
