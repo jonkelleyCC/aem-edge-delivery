b090324a1bc445490bfb5e804f2a9ecf386af860
Jon Kelley <jkelley@crescendocollective.com>
2024-11-08 11:08:38 -0600

    modified card js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -6,11 +6,21 @@ export default function decorate(block) {
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
+    const textContainer = document.createElement('div');
+    textContainer.className = 'cards-card-body';
     moveInstrumentation(row, li);
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
-      else div.className = 'cards-card-body';
+      if (div.children.length === 1 && div.querySelector('picture')) {
+        div.className = 'cards-card-image';
+        div.after(textContainer);
+      } else if (div.querySelector('[data-aue-prop]="subtitle"')) {
+        div.className = 'cards-card-subtitle';
+        textContainer.appendChild(div);
+      } else if (div.querySelector('[data-aue-prop]="title"')) {
+        div.className = 'cards-card-title';
+        textContainer.appendChild(div);
+      } else div.className = 'cards-card-text';
     });
     ul.append(li);
   });
