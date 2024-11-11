09f4df512595c58cd76c8324ad18393e02ae3987
Jon Kelley <jkelley@crescendocollective.com>
2024-11-11 16:39:10 -0600

    added day description to block defs

--- blocks/day-description/day-description.js
+++ blocks/day-description/day-description.js
@@ -19,12 +19,14 @@ export async function loadFragment(path) {
 }
 
 export default async function decorate(block) {
-  const link = block.querySelector('a');
-  const path = link ? link.getAttribute('href') : block.textContent.trim();
-  const fragment = await loadFragment(path);
-  if (fragment) {
-    // const container = block.querySelector('>div');
-    // eslint-disable-next-line no-console
-    console.log(fragment);
-  }
+  // eslint-disable-next-line no-console
+  console.log(block);
+  // const link = block.querySelector('a');
+  // const path = link ? link.getAttribute('href') : block.textContent.trim();
+  // const fragment = await loadFragment(path);
+  // if (fragment) {
+  //   // const container = block.querySelector('>div');
+  //   // eslint-disable-next-line no-console
+  //   console.log(fragment);
+  // }
 }
