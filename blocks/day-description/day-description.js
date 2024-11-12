5bfaa36a25d7d24c630fe380b3035960df2c1a5b
Jon Kelley <jkelley@crescendocollective.com>
2024-11-12 16:24:52 -0600

    day description js change

--- blocks/day-description/day-description.js
+++ blocks/day-description/day-description.js
@@ -6,24 +6,29 @@
 //   loadSections,
 // } from '../../scripts/aem.js';
 
+// eslint-disable-next-line consistent-return
 export async function loadFragment(path) {
   if (path && path.startsWith('/')) {
     // eslint-disable-next-line no-param-reassign
-    path = path.replace(/(\.plain)?\.html/, '').replace('/content/dam', '');
-    const resp = await fetch(`/api/assets${path}.json`);
-    if (resp.ok) {
-      return resp.json();
+    path = path.replace(/(\.plain)?\.html/, '');
+    const resp = await fetch(`${path}.infinity.json`);
+    if (!resp.ok) {
+      throw new Error(`Response status: ${resp.status}`);
     }
+
+    const json = await resp.json();
+    return json['jcr:content']?.data?.master || {};
   }
-  return null;
 }
 
 export default async function decorate(block) {
   // eslint-disable-next-line no-console
   console.log(block);
-  // const link = block.querySelector('a');
-  // const path = link ? link.getAttribute('href') : block.textContent.trim();
-  // const fragment = await loadFragment(path);
+  const link = block.querySelector('a');
+  const path = link ? link.getAttribute('href') : block.textContent.trim();
+  const fragment = await loadFragment(path);
+  // eslint-disable-next-line no-console
+  console.log(fragment);
   // if (fragment) {
   //   // const container = block.querySelector('>div');
   //   // eslint-disable-next-line no-console
