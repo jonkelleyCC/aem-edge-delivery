8e482f6d303ce6420e448a170e1297cdb893369b
Jon Kelley <jkelley@crescendocollective.com>
2024-11-12 16:52:38 -0600

    day description html via js

--- blocks/day-description/day-description.js
+++ blocks/day-description/day-description.js
@@ -29,9 +29,37 @@ export default async function decorate(block) {
   const fragment = await loadFragment(path);
   // eslint-disable-next-line no-console
   console.log(fragment);
-  // if (fragment) {
-  //   // const container = block.querySelector('>div');
-  //   // eslint-disable-next-line no-console
-  //   console.log(fragment);
-  // }
+
+  if (fragment) {
+    const container = block.querySelector('>div');
+    // empty container of all contents including the anchor element
+    container.replaceChildren();
+
+    const imageContainer = document.createElement('div');
+    imageContainer.classList.add('image-container');
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('text-container');
+    container.appendChild(imageContainer);
+    container.appendChild(textContainer);
+
+    const image = document.createElement('img');
+    image.classList.add('day-description__image');
+    image.src = fragment?.image;
+    imageContainer.appendChild(image);
+
+    const subtitle = document.createElement('p');
+    subtitle.classList.add('day-description__subtitle');
+    subtitle.innerText = fragment?.dayNumber;
+    textContainer.appendChild(subtitle);
+
+    const title = document.createElement('h4');
+    title.classList.add('day-description__title');
+    title.innerText = fragment?.destination;
+    textContainer.appendChild(title);
+
+    const description = document.createElement('p');
+    description.classList.add('day-description__description');
+    description.innerText = fragment?.description;
+    textContainer.appendChild(description);
+  }
 }
